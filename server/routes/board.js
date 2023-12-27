33c603b32bcec13c11f29a5ee579a5097af09bee
KrillM <mks160615@gmail.com>
2023-12-27 20:49:34 +0900

    board 테이블 라우팅 기능 작성

---
+++ server/routes/board.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+// const board = require("../controller/board");
+
+// 게시글 페이지 조회
+// router.get("/:boardId", board.boardPage);
+
+// 게시글 검색
+// router.get("/search", board.search);
+
+// 게시글 작성 페이지
+// router.get("/create", board.writePage);
+
+// 게시글 작성
+// router.update("/create", board.createBoard);
+
+// 게시글 수정
+// router.patch("/update/:boardId", board.updateBoard);
+
+// 게시글 삭제
+// router.delete("/delete/:boardId", board.deleteBoard);
+
+// 게시글 추천(좋아요) 기능
+// router.patch("board/like/:boardId", board.boardLike);
+
+module.exports = router;
