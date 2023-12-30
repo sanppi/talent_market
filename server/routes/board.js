c6906328bc5b5ef47ceaa0b6481696a56cac2b8d
명길식 <myeong-gilsig@MBook.local>
2023-12-31 02:11:48 +0900

    Fix: 라우터 연결 이슈 해결

--- server/routes/board.js
+++ server/routes/board.js
@@ -2,21 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Cboard = require("../controller/Cboard");
 
+// 게시글 작성
 router.post("/create", Cboard.boardCreate);
 
-
-
 // 게시글 페이지 조회
-// router.get("/:boardId", Cboard.boardPage);
-
-// 게시글 검색
-// router.get("/search", Cboard.search);
-
-// 게시글 작성 페이지
-// router.get("/create", Cboard.writePage);
-
-// 게시글 작성
-// router.update("/create", Cboard.createBoard);
+// router.get("/:boardId", Cboard.boardDetailPage);
+router.get("/:boardId", function(req, res){
+    Cboard.boardDetailPage
+});
 
 // 게시글 수정
 // router.patch("/update/:boardId", Cboard.updateBoard);
