c26c7b764a2a6bb688a491e92bd0defc80257a7b
riverhye <uhzigani@gmail.com>
2024-01-02 18:59:50 +0900

    Feat: redux로 세션 저장, Design: mypage

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,49 +1,54 @@
-const { Board } = require("../model");
-const { Op } = require("sequelize");  // 추가된 부분
+const { Board } = require('../model');
+const { Op } = require('sequelize'); // 추가된 부분
 
 // 메인 화면
-exports.index = async(req, res) => {
+exports.index = async (req, res) => {
   // res.render("index");
-  try{
+  try {
     const boards = await Board.findAll();
     // console.log(boards);
-    res.json({ products: boards })
-  }
-  catch(error){
-    console.log("에러 코드 ", error);
-    res.status(500).send("홈페이지에 접근할 수 없습니다.")
+    res.json({ products: boards });
+  } catch (error) {
+    console.log('에러 코드 ', error);
+    res.status(500).send('홈페이지에 접근할 수 없습니다.');
   }
 };
 
 exports.search = async (req, res, next) => {
   const words = req.query.search;
   const category = req.query.category; // 카테고리를 query에서 받아옵니다.
-  
+
   try {
     let conditions = [
       {
-        title: { [Op.like]: '%' + words + '%' }
+        title: { [Op.like]: '%' + words + '%' },
       },
       {
-        content: { [Op.like]: '%' + words + '%' }
-      }
+        content: { [Op.like]: '%' + words + '%' },
+      },
     ];
-  
+
     // 만약 카테고리가 존재한다면, conditions에 카테고리 조건을 추가합니다.
-    if(category) {
+    if (category) {
       conditions.push({
-        category: { [Op.eq]: category }
+        category: { [Op.eq]: category },
       });
     }
-  
+
     const results = await Board.findAll({
       where: {
-        [Op.or]: conditions
-      }
+        [Op.or]: conditions,
+      },
     });
-  
+
     res.json(results);
   } catch (err) {
     next(err);
   }
-}
+};
+
+exports.getSession = async (req, res) => {
+  const user = req.session.user;
+  console.log('user', user);
+  res.send({ result: true, user: user });
+};
