59e1312031d0cdc070c6a356a4286e7e54c8224c
riverhye <uhzigani@gmail.com>
2024-01-03 15:57:20 +0900

    Style: 액션 함수 깔끔하게

--- client/src/module/action/authActions.js
+++ client/src/module/action/authActions.js
@@ -1,12 +1,10 @@
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGOUT = 'LOGOUT';
 
-export const loginSuccess = (userData) => {
-  return {
-    type: LOGIN_SUCCESS,
-    payload: userData,
-  };
-};
+export const loginSuccess = (userData) => ({
+  type: LOGIN_SUCCESS,
+  payload: userData,
+});
 
 export const logout = () => ({
   type: LOGOUT,
