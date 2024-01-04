058f4b99f02e1004d326e5304ed9c4f5dcc30e66
riverhye <uhzigani@gmail.com>
2024-01-04 14:49:05 +0900

    Logic: 회원수정

--- client/src/components/pages/mypage/MyPage.js
+++ client/src/components/pages/mypage/MyPage.js
@@ -8,16 +8,14 @@ function MyPage({ user }) {
   const { memberId, nickname, id, redCard } = user;
   const navigate = useNavigate();
   const myDataList = ['찜 목록', '판매 상품', '내 리뷰', '채팅'];
-
-  // Q : 리덕스에 저장한 유저의 로그인 정보가 백과 동일한지 확인 작업 필요?
-  // --- 마이페이지에서 회원정보 받기 ---
-  // 아래와 같은 로직으로 값 체크 후 로그인 -> 마이페이지로
+  //
 
   // --- 마이페이지에서 회원정보 수정 페이지 이동 로직 ---
-  // 1. FE) 리덕스에 저장된 memberId 담아서 서버에 요청 (새로운 API)
-  // 2. BE) 받아온 memberId가 세션과 일치하는지 비교
-  // -> if( === ) 유저 찾기 findOne
-  // -> 결과 전달({result, userData})
+  // 1. FE) {type: 'nickname', 'email', 'pw'
+  // ㄴtype이 pw(비밀번호 변경)이라면 data도 보냄(data: 'oldPw': ~~, 'newPw': ~~)
+  // 2. BE)
+  // -> if(type==='~~') 유저 찾기 findOne
+  // -> 결과 전달({result, userData} 통일)
   // 3. FE) 받아온 정보를 라우터 이동하며 같이 전달해서 Update 화면에서 뿌리기
   //   const handleUserUpdate = async() => {
   //     try {
