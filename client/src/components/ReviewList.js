a1a844c473d93d50e087b95a4a08253bea925f06
riverhye <uhzigani@gmail.com>
2024-01-07 03:04:46 +0900

    Try: 마이페이지 Review 데이터바인딩

--- client/src/components/ReviewList.js
+++ client/src/components/ReviewList.js
@@ -1,41 +1,40 @@
-export default function ReviewList({
-  reviews,
-  selectedReview,
-  setSelectedReview,
-}) {
-  // reviewData = {
-  //     title: reviewTitle,
-  //     memberId: memberId,
-  //     review: reviewContent,
-  //     stars: reviewRating,
-  //     boardId: boardId,
-  //     isAnonymous: isAnonymous,
-  //   };
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+export default function ReviewList({ reviews }) {
+  const reviewArray = Array.isArray(reviews) ? reviews : [reviews];
+
+  // const formattedReviews = reviews.map((review) => ({
+  //   review: review.review,
+  //   title: review.title,
+  //   isAnonymous: review.isAnonymous,
+  //   stars: review.stars,
+  //   boardId: review.Board.boardId,
+  //   memberId: review.Member.memberId,
+  // }));
+  const memberId = useSelector((state) => state.auth.memberId);
+  const [selectedReview, setSelectedReview] = useState(null);
 
   return (
     <>
-      {console.log(reviews)}
-      {/* {reviews === undefined ? (
-        <p>로딩 중...</p>
-      ) : reviews.length === 0 ? (
+      {reviewArray.length === 0 ? (
         <div className="noReviewNotice">아직 작성된 리뷰가 없습니다.</div>
       ) : (
-        reviews.map((review, index) => (
+        reviewArray.map((review, index) => (
           <div key={review.commentId}>
             <div
               onClick={() =>
                 setSelectedReview(selectedReview !== index ? index : null)
               }
             >
-              {index + 1}. {review.title} -{' '}
-              {review.isAnonymous ? '익명' : review.Member.nickname} (
+              {index + 1}. {review.title} - {review.isAnonymous ? '익명' : ''} (
               {new Date(review.createdAt).toLocaleDateString()}){' '}
               {'★'.repeat(review.stars)}
             </div>
             {selectedReview === index && <p>{review.review}</p>}
           </div>
         ))
-      )} */}
+      )}
     </>
   );
 }
