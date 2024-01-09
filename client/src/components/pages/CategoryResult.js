4a877c9ea755b6dc895836906cffb995a8472d1b
riverhye <uhzigani@gmail.com>
2024-01-09 20:53:30 +0900

    Design: 카테고리 오류 수정

--- client/src/components/pages/CategoryResult.js
+++ client/src/components/pages/CategoryResult.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useLocation } from "react-router-dom";
-import { ProductCard } from "./Main.js";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+import { ProductCard } from './Main.js';
 
 export default function CategoryResult() {
   const [products, setProducts] = useState([]);
   const location = useLocation();
-  const category = new URLSearchParams(location.search).get("category");
+  const category = new URLSearchParams(location.search).get('category');
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -16,7 +16,7 @@ export default function CategoryResult() {
         );
         setProducts(response.data.products);
       } catch (error) {
-        console.error("Cannot fetch products", error);
+        console.error('Cannot fetch products', error);
       }
     };
 
@@ -24,7 +24,7 @@ export default function CategoryResult() {
   }, [category]);
 
   return (
-    <>
+    <div className="categoryList">
       <span className="categoryNotice">
         {category} 카테고리 상품이 {products.length}개 존재합니다.
       </span>
@@ -33,6 +33,6 @@ export default function CategoryResult() {
           <ProductCard key={product.boardId} product={product} />
         ))}
       </div>
-    </>
+    </div>
   );
 }
